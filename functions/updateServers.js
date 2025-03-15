1fec83a4619d6b25f292437981c70a425ad54504
RahulR100 <rahulrao@email.com>
2025-03-15 21:29:45 +0000

    remove update delay

--- functions/updateServers.js
+++ functions/updateServers.js
@@ -17,50 +17,37 @@ function handleUpdateError(error, ip, guild) {
 	);
 }
 
-async function mapWithDelay(array, callback, delay) {
-	const results = [];
-	for (const item of array) {
-		results.push(await callback(item));
-		await new Promise((resolve) => setTimeout(resolve, delay));
-	}
-	return results;
-}
-
 export async function updateServers(client) {
 	await Promise.allSettled(
 		client.guilds.cache.map(async (guild) => {
 			let serverList = await getServers(guild.id);
 
 			await Promise.allSettled(
-				mapWithDelay(
-					serverList,
-					async (server) => {
-						let serverStatus;
-						let serverError;
+				serverList.map(async (server) => {
+					let serverStatus;
+					let serverError;
 
-						try {
-							serverStatus = await getServerStatus(server, 'low_priority');
-						} catch (error) {
-							if (error.message == 'Invalid server IP') {
-								serverError = 'Invalid IP Address';
-							} else {
-								handleUpdateError(error, server.ip, guild.id);
-							}
+					try {
+						serverStatus = await getServerStatus(server, 'low_priority');
+					} catch (error) {
+						if (error.message == 'Invalid server IP') {
+							serverError = 'Invalid IP Address';
+						} else {
+							handleUpdateError(error, server.ip, guild.id);
 						}
+					}
 
-						if (!serverStatus && !serverError) return;
+					if (!serverStatus && !serverError) return;
 
-						const channels = [
-							{ object: await guild.channels.cache.get(server.statusId), type: 'status' },
-							{ object: await guild.channels.cache.get(server.playersId), type: 'players' }
-						];
+					const channels = [
+						{ object: await guild.channels.cache.get(server.statusId), type: 'status' },
+						{ object: await guild.channels.cache.get(server.playersId), type: 'players' }
+					];
 
-						const indicators = await getIndicators(guild.id, server);
+					const indicators = await getIndicators(guild.id, server);
 
-						await renameChannels(channels, serverStatus, indicators, 'low_priority', serverError);
-					},
-					10
-				)
+					await renameChannels(channels, serverStatus, indicators, 'low_priority', serverError);
+				})
 			);
 		})
 	);
